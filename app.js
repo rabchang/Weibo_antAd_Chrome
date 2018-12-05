b06ab9c56f6c02e6cdd4c60d33b1e78891e1aa9e
Rab Chang <zhanghcdream@outlook.com>
2018-12-05 23:21:23 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+$(function() 
+{
+  setTimeout(()=>
+  {
+    $("v6_pl_content_publishertop textarea").val('吃最甜的瓜 做最酷的仔');
+  },2000);
+  /*
+  console.log("loaded");
+
+  console.log($("#v6_pl_rightmod_attfeed"));
+  $("#v6_pl_rightmod_attfeed").remove();
+
+  setTimeout( ()=>
+  {
+  	$("#v6_pl_rightmod_attfeed").remove();
+  	console.log("找不熬~");
+  } , 2000 );
+
+  */
+  
+});
